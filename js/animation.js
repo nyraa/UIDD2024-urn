da322f7a248ddcb190d446f59cc88a760baefe99
nyraa <112930946+nyraa@users.noreply.github.com>
2024-04-05 07:19:35 +0800

    undo animation trigger strict condition

--- js/animation.js
+++ js/animation.js
@@ -1,18 +1,14 @@
 window.addEventListener("load", function() {
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-            const { intersectionRect, boundingClientRect, target } = entry;
-            const isFullyVisible = intersectionRect.top >= boundingClientRect.top && intersectionRect.bottom <= boundingClientRect.bottom;
-            if (entry.isIntersecting && isFullyVisible) {
+            if (entry.isIntersecting) {
                 const target = entry.target;
                 target.classList.add("animated");
                 observer.unobserve(target);
             }
         },
         {
-            threshold: 1,
-            rootMargin: "-50%",
-            root: null
+            threshold: 0.1
         });
     });
     const elements = document.querySelectorAll(".animation-required");
