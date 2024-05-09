904ed311575f438fa8709e8c550581ac5a194f17
nyraa <112930946+nyraa@users.noreply.github.com>
2024-05-10 03:21:45 +0800

    add homepage action links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,6 +12,7 @@ export default function Home() {
             <Nav title={true} />
             <HomeBanner />
             <FindYourLove />
+            <ActionLinks />
             <Splitter>以不同類別探索靈骨塔</Splitter>
             <Splitter>今日逝世紀念</Splitter>
         </>
@@ -30,5 +31,25 @@ function FindYourLove() {
             </div>
             <SearchBox />
         </section>
+    )
+}
+
+function ActionLinks() {
+    return (
+        <section className="action-links">
+            <ActionLink title="協助創建塔位" line1="AI 輔助個人事蹟創建" line2="助您珍藏在世寶貴回憶" />
+            <ActionLink title="AI悼念互動" line1="搜尋全球首創" line2="AI 互動線上靈塔" />
+        </section>
+    )
+}
+
+function ActionLink({ title, line1, line2, href }) {
+    return (
+        <div className="action-link">
+            <img className="logo" src="assets/logo.png" />
+            <div className="intro-title">{title}</div>
+            <div className="hr"></div>
+            <div className="intro-text">{line1}<br />{line2}</div>
+        </div>
     )
 }
