bcbde736bdc785219cfb2d830e5590c51dd5ff1d
nyraa <112930946+nyraa@users.noreply.github.com>
2024-05-10 06:40:28 +0800

    add anchor to title logo

--- src/app/components/Title.jsx
+++ src/app/components/Title.jsx
@@ -2,8 +2,8 @@ import "@app/components/Title.sass";
 
 export default function Title({ children }) {
     return (
-        <div className="logo-title">
+        <a className="logo-title" href="/">
             {children}
-        </div>
+        </a>
     )
 }
