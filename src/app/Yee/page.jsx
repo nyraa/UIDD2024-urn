d84c557ec38662f4ee0d43a988735724ffd11fb0
Max <user@gmail.com>
2024-06-19 02:20:02 +0800

    add flower and adjust xiang

--- src/app/Yee/page.jsx
+++ src/app/Yee/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import Nav from "@app/components/Nav";
+import {PrismaClient} from '@prisma/client';
+ const prisma = new PrismaClient();
 import Splitter from "@app/components/Splitter";
 import React, { useEffect, useState, useRef } from 'react';
 import movies from "./peoplepicture.json";
@@ -7,94 +9,109 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Yee.sass";
-export default function Finalpage() {
-    const [positions, setPositions] = useState({});
-    const slidesRef = useRef(null);
-    const currentIndexRef = useRef(0);
+
+ const ID = 'clxbkpi7y000454fu4bdatqey'; // 替換ID 
+
+
+export default  function Finalpage() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        console.log(`Fetching data with ID: ${ID}`);
+        const response = await fetch(`/api/finalpage?id=${ID}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        if (result.error) {
+          throw new Error(result.error);
+        }
+        console.log('Fetched data:', result); // 调试日志
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+        console.error('Error fetching the data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+    return (
+        <>
+            <Nav title={true} />
+            <Background />
+            <Goldenword children={data}/> 
+            <Flower/>           
+            <Photoshot children={data}/>
+            
+            <Name children={data}/>
+            <About children={data}/>
+            <Slidepicture/>
+            <Story children={data}/>
+        </>
+    );
+
+};
+
+function Background(){
+     const [positions, setPositions] = useState({});
     
     useEffect(() => {
         const setBack2Position = () => {
+          //對齊背景
           const back1Height = document.querySelector('.back1').offsetHeight;
           const back2Height = document.querySelector('.back2').offsetHeight;
           const mountainHeight = document.querySelector('.mountain').offsetHeight;
 
-    
           const TopPosition = back1Height - (back2Height / 2);
           const BottomPosition = back1Height - mountainHeight;
-
-    
           setPositions({
             back2: TopPosition,
             mountain: BottomPosition,
           });
+
+
         };
-    
+    //window及時刷新
         window.addEventListener('load', setBack2Position);
         window.addEventListener('resize', setBack2Position);
-    
         setBack2Position();
-    
         return () => {
           window.removeEventListener('load', setBack2Position);
           window.removeEventListener('resize', setBack2Position);
         };
       }, []);
-    
-      useEffect(() => {
-        const nextSlide = () => {
-          if (slidesRef.current) {
-            const slides = slidesRef.current;
-            const slideItems = slides.querySelectorAll('.slide');
-            currentIndexRef.current = (currentIndexRef.current + 1) % slideItems.length;
-            slides.style.transform = `translateX(-${currentIndexRef.current * 100}%)`;
-          }
-        };
-    
-        const interval = setInterval(nextSlide, 3000); // 每隔3秒切换到下一张幻灯片
-        return () => clearInterval(interval);
-      }, []);
-    
-    return (
-        <>
-            <Nav title={true} />
-            <Background children={positions}/>
-            <Goldenword/>            
-            <Photoshot />
-            <Name/>
-            <About/>
-            <Slidepicture/>
-            <Story/>
-        </>
-    );
-
-};
-function Background({children}){
+     
   return(
     <section className="backgrounds">
       <img className="back1" src="./picture/background.png"/>  
-      <img className="back2" style={{ top: children.back2 }} src="./picture/Rectangle 110.png"/>
-      <img className="mountain" style={{ top: children.mountain }} src="./picture/Mountain.png"/>
+      <img className="back2" style={{ top: positions.back2 }} src="./picture/Rectangle 110.png"/>
+      <img className="mountain" style={{ top: positions.mountain }} src="./picture/Mountain.png"/>
       <div className="back3"></div>
       <img className="cloud" src="./picture/cloud.png"/>
     </section>
   );
 }
 
-function Photoshot(){
+function Photoshot({children}){
   return(
     <section className="photoshot">
-    <Mainphoto/>
+    <Mainphoto picturedata={children}/>
     <Bonebox/>
     <Smoke/>
     </section>
   );
 }
 
-function Mainphoto(){
+function Mainphoto({picturedata}){
     return(
         <section className="picture">
-        <img className="people" src="./picture/people.png"/>      
-        {/* <img className="behind-people" src="./picture/Ellipse 37.png"/> */}
+        <img className="people" src={ picturedata?.cover_src }/>      
+        <img className="behind-people" src="./picture/Ellipse 37.png"/>
         </section>
     );
 }
@@ -107,23 +124,130 @@ function Bonebox(){
   );
 }
 function Smoke(){
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [showImages, setShowImages] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(60);
+  const intervalRef = useRef(null);
+
+  // 點擊按鈕變更滑鼠形狀
+  const handleButtonClick = () => {
+    setIsButtonClicked(true);
+  };
+
+  // 點擊圖片 A 顯示圖片 B, C 和 D 並開始計時，或者顯示剩餘時間
+  const handleImageAClick = () => {
+    if (isButtonClicked) {
+      setShowImages(true);
+      setIsButtonClicked(false);
+      if (!intervalRef.current) {
+        startTimer();
+      }
+    } else if (showImages && intervalRef.current) {
+      alert(`Remaining time: ${remainingTime} seconds`);
+    }
+  };
+
+  // 開始計時
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      setRemainingTime((prev) => {
+        if (prev === 1) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+          setShowImages(false);
+          return 60;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
+
+  // 清理計時器
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return(
-    <section className="smoke">
-      <img className="xianglu" src="./picture/xianglu.png"/>
+    <section className="smoke" style={{ cursor: isButtonClicked ? 'url("/picture/ig.png"), auto' : 'default' }} >
+       <button onClick={handleButtonClick} className="click">Click me</button>
+       <div 
+        onClick={handleImageAClick} 
+      >
+       <div onClick={handleImageAClick}>
+       <img className="xianglu" src="./picture/xianglu.png"/>
+      </div>
+      {showImages && (
+        <>
+        <img className="image_b" src="./picture/xiang.png" alt="Image B" />
+        <img className="image_c" src="./picture/Ellipse 38.png" alt="Image C" />
+        <img className="image_d" src="./picture/Ellipse 38.png" alt="Image D" />
+        </>
+      )}
+      </div>
     </section>
   );
 }
-function Goldenword(){
+function Flower(){
+  const [count, setCount] = useState(0);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // 確認這段程式碼在瀏覽器中運行
+    setIsClient(true);
+    if (typeof window !== 'undefined') {
+      const savedCount = localStorage.getItem('count');
+      if (savedCount !== null) {
+        setCount(parseInt(savedCount, 10));
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem('count', count);
+    }
+  }, [count, isClient]);
+
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+
+  return(
+    <section className="flowers">
+      <h1>Counter: {count}</h1>
+      <button className="cl1" onClick={handleIncrement}>Increment</button>
+      <button className="cl2" onClick={handleReset}>Reset</button>
+      {count >= 10 && (
+        <div>
+          <img className="flower1" src="./picture/flower1.png" alt="Flower 1" />
+          <img className="flower2" src="./picture/flower2.png" alt="Flower 2" />
+          <img className="flower4" src="./picture/flower4.png" alt="Flower 4" />
+          <img className="flower3" src="./picture/flower3.png" alt="Flower 3" />
+        </div>
+      )}
+    </section>
+
+  );
+}
+function Goldenword({children}){
     return(
-        <div className = "goldenword"><p>哩娘卡好！</p></div>
+        <div className = "goldenword"><p>{children ? children.golden_quote : 'Loading...'}</p></div>
     );
 };
-function Name(){
+ function Name({children}){
+
     return(
         <section className="name" >
-          <p className="n1">豬哥亮</p>
-          <p className="n2">永遠的秀場之王</p>
-          <p className="n2">1985-2024</p>
+          <p className="n1">{children ? children.name : 'Loading...'}</p>
+          <p className="n2">{children ? children.title : 'Loading...'}</p>
+          <p className="n2">{children ? children.born_date : 'Loading...'}-{children ? children.death_date : 'Loading...'}</p>
           <Share/>
         </section>
     );
@@ -143,35 +267,26 @@ return(
 );
 }
 
-function About (){
+function About ({children}){
   return(
     <section className="detail"  >
-      <p id="d1">豬哥亮是一位著名的台灣喜劇演員，
-        以其獨特的幽默風格和深具感染力的表演而聞名，
-        他在自己的演藝生涯中積極推廣笑聲，為觀眾帶來歡樂。
-        </p>
+      <p id="d1">
+      {children ? children.urn_texture_src : 'Loading...'}
+      </p>
       <p id="d2">逝世</p>
       <p id="d1">預估2024年5月17日</p>
       <p id="d2">死亡詳情</p>
-      <p id="d1">在台灣的家中因疾病去世,享年72歲。</p>
+      <p id="d1">{children ? children.last_live_city : 'Loading...'}</p>
     </section>
   );
 };
 
-function Story(){
+function Story({children}){
   return(
     <section className="mainstory" >
         <div className="s1"> 生命故事</div>
         <div className="s2">
-            豬哥亮，原名周慶煌,1949年生於台灣彰化縣，是台灣娛樂界的傳奇人物之一。他的生命故事充滿了戲劇性和鼓舞人心的奮鬥精神。
-            <br/><br/><br/><br/>
-            年輕時，豬哥亮並沒有一帆風順的成長經歷。他出生在一個貧困的家庭，面對著生活的種種困難，但他從不放棄對夢想的追求。年輕時期，他曾從事過各種工作，包括農夫、水電工、修車工等，生活十分艱辛。
-            <br/><br/><br/><br/>
-            然而，豬哥亮的命運在1970年代發生了轉折。當時，他加入了知名的「喜劇雙人組」，與王傑搭檔演出，成為了台灣笑匠的代表之一。他們以滑稽搞笑的風格在電視節目中大放異彩，獲得了觀眾的熱烈喜愛，開啟了豬哥亮輝煌的演藝生涯。
-            豬哥亮不僅在電視節目中表現出色，在電影界也嶄露頭角。他參演了許多經典喜劇電影，如《賭聖》、《賭俠》等，飾演的角色深入人心，成為了經典中的經典。
-            <br/><br/><br/><br/>
-            除了他的演藝事業外，豬哥亮還是一位熱心的社會公益人士。他積極參與各種慈善活動，關心社會弱勢群體，用自己的行動影響著更多人。
-            豬哥亮的一生充滿了汗水、淚水和笑聲，他用自己的努力和才華，從一個平凡的人走向了台灣娛樂界的巔峰。他的生命故事鼓舞著我們，告訴我們只要努力奮鬥，就一定能夠實現夢想。
+        {children ? children.life_story : 'Loading...'}
         </div>
     </section>
   );
---
+++ src/pages/api/finalpage.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    const { id } = req.query;
+
+  try {
+    const data = await prisma.morgue.findUnique({
+      where: 
+        { id }, // 替换为你实际的Morgue ID
+      
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
